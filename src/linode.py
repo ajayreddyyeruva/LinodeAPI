d69b3760fa8d036478b7ceaa11bfc543793891a4
sandy <sandeep_r80@yahoo.co.in>
2014-02-15 20:02:13 +0530

    Fixed linode boot issue

--- src/linode.py
+++ src/linode.py
@@ -27,52 +27,62 @@ class Linode(object):
         self.paymentTerm=1
 
     def _createLinode(self):
-        self.linodeId=[l['LINODEID'] for l in  self.linode.linode_list() if (self.linodeIdentifier in l['LABEL'])][0]
+        self.linodeId=[l['LINODEID'] for l in  self.linode.linode_list() if (self.linodeIdentifier in l['LABEL'])]
         if not self.linodeId:
             print "Lindoe doesn't exists. Creating it"
             linodeNode=self.linode.linode_create(DatacenterID=self.dallasDataCenterId, PlanID=self.planId, PaymentTerm=self.paymentTerm)
             self.linodeId=linodeNode['LinodeID']
             self.linode.linode_update(LinodeID=self.linodeId, Label=self.linodeIdentifier,lpm_displayGroup=self.config.get('DEFAULT','LINODE_GROUP'))
+        else:
+            self.linodeId=self.linodeId[0]
             
             
         print self.linodeId
     
     def _createRootDiskIfNotExist(self):
-        self.rootDiskId=[d['DISKID'] for d in  self.linode.linode_disk_list(LinodeID=self.linodeId) if ('Root Partition' in d['LABEL'])][0]
+        self.rootDiskId=[d['DISKID'] for d in  self.linode.linode_disk_list(LinodeID=self.linodeId) if ('Root Partition' in d['LABEL'])]
         if not self.rootDiskId:
             print "Root disk doesn't exists creating 20 GB of it"
             self.disk=self.linode.linode_disk_createfromdistribution(LinodeID=self.linodeId, DistributionID=self.distributionId, Label='Root Partition', Size=(self.config.getint('DEFAULT', 'ROOT_DISK_SIZE')), rootPass=self.config.get('DEFAULT','ROOT_PWD'),rootSSHKey=open(self.config.get('DEFAULT','ROOT_SSH_KEY')).read())
             self.rootDiskId=self.disk['DiskID']
+        else:
+            self.rootDiskId=self.rootDiskId[0]
         print self.rootDiskId
     
     def _createSwapDiskIfNotExist(self):
-        self.swapDiskId=[d['DISKID'] for d in  self.linode.linode_disk_list(LinodeID=self.linodeId) if ('Swap Partition' in d['LABEL'])][0]
+        self.swapDiskId=[d['DISKID'] for d in  self.linode.linode_disk_list(LinodeID=self.linodeId) if ('Swap Partition' in d['LABEL'])]
         if not self.swapDiskId:
             print "Swap disk doesn't exists creating 2 GB of it"
             self.disk=self.linode.linode_disk_create(LinodeID=self.linodeId, Label='Swap Partition', Type='swap', Size=self.config.getint('DEFAULT', 'SWAP_DISK_SIZE'))
             self.swapDiskId=self.disk['DiskID']
+        else:
+            self.swapDiskId=self.swapDiskId[0]
         print self.swapDiskId
         
     def _createConfigIfNotExist(self):
-        self.configId=[c['ConfigID'] for c in  self.linode.linode_config_list(LinodeID=self.linodeId) if ('Monimus Default Config' in c['Label'])][0]
+        self.configId=[c['ConfigID'] for c in  self.linode.linode_config_list(LinodeID=self.linodeId) if ('Monimus Default Config' in c['Label'])]
         if not self.configId:
             print "Configuration doesn't exists creating it"
             self.config=self.linode.linode_config_create(LinodeID=self.linodeId, KernelID=self.kernelId, Label='Monimus Default Config', DiskList=[self.rootDiskId,self.swapDiskId])
             self.configId=self.config['ConfigID']
+        else:
+            self.configId=self.configId[0]
         print self.configId
     
     def _addPrivateIp(self):
-        self.pvtIpAddress=[i['IPADDRESS'] for i in  self.linode.linode_ip_list(LinodeID=self.linodeId) if (not i['ISPUBLIC'])][0]
+        self.pvtIpAddress=[i['IPADDRESS'] for i in  self.linode.linode_ip_list(LinodeID=self.linodeId) if (not i['ISPUBLIC'])]
         if not self.pvtIpAddress:
             print "This machine doesn't have a private IP address adding a private IP"
             self.pvtIpAddress=self.linode.linode_ip_addprivate(LinodeID=self.linodeId)
+        else:
+            self.pvtIpAddress=self.pvtIpAddress[0]
         print self.pvtIpAddress
             
     def _bootLinode(self):
         linodeNode=self.linode.linode_list(LinodeID=self.linodeId)[0]
         if not linodeNode['STATUS']:
             print ("Linode is not running, so booting it up!")
-            self.linode.linode_boot(self.linodeId)
+            self.linode.linode_boot(LinodeID=self.linodeId,ConfigID=self.configId)
 
     def create(self):
         self._createLinode()
@@ -83,5 +93,5 @@ class Linode(object):
         self._bootLinode()
     
 if __name__ == "__main__":
-    saLinode=Linode('bs1_monimus_org')
+    saLinode=Linode('bs2_monimus_org')
     saLinode.create()
