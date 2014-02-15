4c1c19682aac70c2a820a22b85c197439bdb8124
sandy <sandeep_r80@yahoo.co.in>
2014-02-15 19:41:33 +0530

    Updated code to user index

--- src/linode.py
+++ src/linode.py
@@ -19,15 +19,15 @@ class Linode(object):
         Constructor
         '''
         self.linodeIdentifier=linodeIdentifier
-        self.distributionId=[d['DISTRIBUTIONID'] for d in self.linode.avail_distributions() if (self.config.get('DEFAULT','UBUNTU_DIST') in d['LABEL']) and (d['IS64BIT'])]
+        self.distributionId=[d['DISTRIBUTIONID'] for d in self.linode.avail_distributions() if (self.config.get('DEFAULT','UBUNTU_DIST') in d['LABEL']) and (d['IS64BIT'])][0]
         print("Distribution id: %s"%(self.config.get('DEFAULT','UBUNTU_DIST')))
-        self.kernelId=[_['KERNELID'] for _ in self.linode.avail_kernels() if (self.config.get('DEFAULT','KERNEL_LABEL') in _['LABEL']) ]
-        self.dallasDataCenterId=[dc['DATACENTERID'] for dc in self.linode.avail_datacenters() if self.config.get('DEFAULT','DATACENTER_LABEL') in dc['LOCATION']]
-        self.planId=[p['PLANID'] for p in self.linode.avail_linodeplans() if self.config.get('DEFAULT','PLAN_ID') in p['LABEL']]
+        self.kernelId=[_['KERNELID'] for _ in self.linode.avail_kernels() if (self.config.get('DEFAULT','KERNEL_LABEL') in _['LABEL']) ][0]
+        self.dallasDataCenterId=[dc['DATACENTERID'] for dc in self.linode.avail_datacenters() if self.config.get('DEFAULT','DATACENTER_LABEL') in dc['LOCATION']][0]
+        self.planId=[p['PLANID'] for p in self.linode.avail_linodeplans() if self.config.get('DEFAULT','PLAN_ID') in p['LABEL']][0]
         self.paymentTerm=1
 
     def _createLinode(self):
-        self.linodeId=[l['LINODEID'] for l in  self.linode.linode_list() if (self.linodeIdentifier in l['LABEL'])]
+        self.linodeId=[l['LINODEID'] for l in  self.linode.linode_list() if (self.linodeIdentifier in l['LABEL'])][0]
         if not self.linodeId:
             print "Lindoe doesn't exists. Creating it"
             linodeNode=self.linode.linode_create(DatacenterID=self.dallasDataCenterId, PlanID=self.planId, PaymentTerm=self.paymentTerm)
@@ -38,7 +38,7 @@ class Linode(object):
         print self.linodeId
     
     def _createRootDiskIfNotExist(self):
-        self.rootDiskId=[d['DISKID'] for d in  self.linode.linode_disk_list(LinodeID=self.linodeId) if ('Root Partition' in d['LABEL'])]
+        self.rootDiskId=[d['DISKID'] for d in  self.linode.linode_disk_list(LinodeID=self.linodeId) if ('Root Partition' in d['LABEL'])][0]
         if not self.rootDiskId:
             print "Root disk doesn't exists creating 20 GB of it"
             self.disk=self.linode.linode_disk_createfromdistribution(LinodeID=self.linodeId, DistributionID=self.distributionId, Label='Root Partition', Size=(self.config.getint('DEFAULT', 'ROOT_DISK_SIZE')), rootPass=self.config.get('DEFAULT','ROOT_PWD'),rootSSHKey=open(self.config.get('DEFAULT','ROOT_SSH_KEY')).read())
@@ -46,7 +46,7 @@ class Linode(object):
         print self.rootDiskId
     
     def _createSwapDiskIfNotExist(self):
-        self.swapDiskId=[d['DISKID'] for d in  self.linode.linode_disk_list(LinodeID=self.linodeId) if ('Swap Partition' in d['LABEL'])]
+        self.swapDiskId=[d['DISKID'] for d in  self.linode.linode_disk_list(LinodeID=self.linodeId) if ('Swap Partition' in d['LABEL'])][0]
         if not self.swapDiskId:
             print "Swap disk doesn't exists creating 2 GB of it"
             self.disk=self.linode.linode_disk_create(LinodeID=self.linodeId, Label='Swap Partition', Type='swap', Size=self.config.getint('DEFAULT', 'SWAP_DISK_SIZE'))
@@ -54,7 +54,7 @@ class Linode(object):
         print self.swapDiskId
         
     def _createConfigIfNotExist(self):
-        self.configId=[c['ConfigID'] for c in  self.linode.linode_config_list(LinodeID=self.linodeId) if ('Monimus Default Config' in c['Label'])]
+        self.configId=[c['ConfigID'] for c in  self.linode.linode_config_list(LinodeID=self.linodeId) if ('Monimus Default Config' in c['Label'])][0]
         if not self.configId:
             print "Configuration doesn't exists creating it"
             self.config=self.linode.linode_config_create(LinodeID=self.linodeId, KernelID=self.kernelId, Label='Monimus Default Config', DiskList=[self.rootDiskId,self.swapDiskId])
@@ -62,14 +62,14 @@ class Linode(object):
         print self.configId
     
     def _addPrivateIp(self):
-        self.pvtIpAddress=[i['IPADDRESS'] for i in  self.linode.linode_ip_list(LinodeID=self.linodeId) if (not i['ISPUBLIC'])]
+        self.pvtIpAddress=[i['IPADDRESS'] for i in  self.linode.linode_ip_list(LinodeID=self.linodeId) if (not i['ISPUBLIC'])][0]
         if not self.pvtIpAddress:
             print "This machine doesn't have a private IP address adding a private IP"
             self.pvtIpAddress=self.linode.linode_ip_addprivate(LinodeID=self.linodeId)
         print self.pvtIpAddress
             
     def _bootLinode(self):
-        linodeNode=self.linode.linode_list(LinodeID=self.linodeId)
+        linodeNode=self.linode.linode_list(LinodeID=self.linodeId)[0]
         if not linodeNode['STATUS']:
             print ("Linode is not running, so booting it up!")
             self.linode.linode_boot(self.linodeId)
